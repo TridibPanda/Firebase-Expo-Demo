8e811c8d8c95ecafeefafea2740114cd79a86748
Tridib Panda <pandatridib@gmail.com>
2021-08-11 13:05:52 +0530

    first commit

--- config/Firebase.js
+++ config/Firebase.js
@@ -16,17 +16,5 @@ var firebaseConfig = {
 let Firebase = firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
 console.log('Cloud Firestores Loaded');
-// // avoid deprecated warnings
-
-db.settings({
-    cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED,
-});
-
-// db.enablePersistence();
-
-// db.settings({
-// 	timestampsInSnapshots: true
-// })
-// storageBucket: 'pitchers-61807.appspot.com',
 
 export default Firebase;
